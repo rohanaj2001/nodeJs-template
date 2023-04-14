95ef9b7dbffead24cd04dbd2a2c33ed4c1e99346
rohanaj2001 <rohanaj2001@gmail.com>
2023-04-14 19:12:16 +0530

    login and jwt

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,17 @@ const auth = require('./auth');
 
 router.use('/health', test.health);
 router.use('/auth', auth);
+
+// Protected API endpoint
+router.get('/api/data', authMiddleware, (req, res) => {
+    // The user ID is attached to the request object by the auth middleware
+    const userId = req.userId;
+
+    // Fetch the user data from the database using the user ID
+    const users = db.collection('users');
+    const user = users.findOne({ _id: userId });
+
+    // Return the user data in the response
+    res.json({ user });
+});
 module.exports = router;
